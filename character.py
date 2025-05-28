02297fbe3b3b43612186ebef3660da9906450bd9
The Bird, Blue <115736630+TheBirdBlue@users.noreply.github.com>
2025-05-28 11:20:55 -0500

    Delete character.py
    Bad file upload

--- character.py
+++
@@ -1,4 +0,0 @@
-# Character TODO
-# 1 - Set up level up for skills, class, and player
-# 2 - Player level up allows for player to select specific attribute to upgrade
-#       HP, MP, TP, Str, Mag, Lck
